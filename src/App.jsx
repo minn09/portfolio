a0f4a6ced17201335ea56f13887b00737bfc1682
Jonathan <jonathan.huertas.contreras@gmail.com>
2024-12-15 09:53:38 -0500

    feat: :lipstick: add global styles in the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ProjectCard from "./components/ProjectCard/ProjectCard.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import Technologies from "./components/Technologies/Technologies.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import "./global.css";
 function App() {
   return (
     <>
