c3de6c38e10eb7004d2dad9b556749d86ed13504
Jonathan <jonathan.huertas.contreras@gmail.com>
2025-01-06 18:18:09 -0500

    feat: :lipstick: change the secondary color

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -3,27 +3,27 @@ import "./technologies.css";
 function Technologies() {
   return (
     <article className="technologies">
-      <h2>Tecnologías</h2>
+      <h2 className="technology-tittle">Tecnologías</h2>
       <div className="technologies-container">
         <div className="technology">
           <img src="/assets/svg/html.svg" alt="HTML" />
-          <p>HTML</p>
+          <p className="technology-text">HTML</p>
         </div>
         <div className="technology">
           <img src="./assets/svg/css.svg" alt="CSS" />
-          <p>CSS</p>
+          <p className="technology-text">CSS</p>
         </div>
         <div className="technology">
           <img src="./assets/svg/js.svg" alt="JavaScript" />
-          <p>JavaScript</p>
+          <p className="technology-text">JavaScript</p>
         </div>
         <div className="technology">
           <img src="./assets/svg/react.svg" alt="React" />
-          <p>React</p>
+          <p className="technology-text">React</p>
         </div>
         <div className="technology">
           <img src="./assets/svg/python.svg" alt="Python" />
-          <p>Python</p>
+          <p className="technology-text">Python</p>
         </div>
       </div>
     </article>
